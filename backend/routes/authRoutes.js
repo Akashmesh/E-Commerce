cb50d55bc1efbf00f217abdd6a0a03231a990fd8
Akashmesh <akashmeshram2486@gmail.com>
2026-01-17 19:20:24 +0530

    created initial react app implemented react router dom tailwind css and builded a navbar

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {logout, register, login} from "../controller/authController.js";
-
-const authRoutes = express.Router();
-authRoutes.post("/register", register);
-authRoutes.post("/login", login);
-authRoutes.post("/logout", logout);
-
-export default authRoutes;
